7887029eb16640872a50f1438b1f15e943fea545
HALA-MUMMO\Hala MUMMO <halashem13@gmail.com>
2022-04-20 02:59:38 +0300

    Enable accessing to CLI Task from the module interrupt file.

--- H26R0/H26R0_it.c
+++ H26R0/H26R0_it.c
@@ -18,7 +18,7 @@ extern uint8_t UARTRxBufIndex[NumOfPorts];
 
 /* External function prototypes ----------------------------------------------*/
 
-
+extern TaskHandle_t xCommandConsoleTaskHandle; // CLI Task handler.
 
 
 /******************************************************************************/
