a177b4c533965ae23c5fab77615add6878c39598
Aula Jazmati <37100066+aula9@users.noreply.github.com>
2024-09-22 20:06:05 +0300

    Update H26R0_gpio.c

--- H26R0/H26R0_gpio.c
+++ H26R0/H26R0_gpio.c
@@ -46,14 +46,15 @@ void HX711_GPIO_Init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStruct;
 
-	GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_3;
+	GPIO_InitStruct.Pin = RATE_pin |PD_SCK ;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed =  GPIO_SPEED_FREQ_HIGH;
 	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-	GPIO_InitStruct.Pin = GPIO_PIN_4;
+	GPIO_InitStruct.Pin = DOUT;
 	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	//GPIO_InitStruct.Pull = GPIO_PULLUP;
 	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
 //-- Configure indicator LED
